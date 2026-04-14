b74bd7cc9a23d5cab29f0d38d924155ee3c1118a
Nik <KupiNik@protonmail.com>
2026-04-14 10:59:52 -0400

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -6,3 +6,11 @@ function saveTransactions() {
 }
 
 // Existing content from lines 114 to end
+// learned commit vanished original long window, lol
+// Add x2 calls and presto, localStorage complete
+// Call 1
+transactions.push(transaction);
+saveTransactions();
+// Call 2
+transactions = transactions.filter(t => t.id !== id);
+saveTransactions();
